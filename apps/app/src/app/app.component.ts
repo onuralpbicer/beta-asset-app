0df5023a77995d206aa875b8e29b60afb6ac1c0f
Onur Alp Bicer <onuralpbicer@gmail.com>
2023-04-25 21:45:41 +1000

    Setup workspace

---
+++ apps/app/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+@Component({
+    selector: 'beta-asset-app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+    constructor() {}
+}
