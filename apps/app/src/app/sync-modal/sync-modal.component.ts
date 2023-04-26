3e5123dea4db694e2e1bda4c9c3b1fd71f58a4c3
Onur Alp Bicer <onuralpbicer@gmail.com>
2023-04-26 23:54:47 +1000

    Revert "Sync modal visuals"
    This reverts commit 77857676f24fdd322fd19862206b6f84df41bc95.

--- apps/app/src/app/sync-modal/sync-modal.component.ts
+++ apps/app/src/app/sync-modal/sync-modal.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core'
-import { IonicModule } from '@ionic/angular'
 
 @Component({
     selector: 'beta-asset-app-sync-modal',
     templateUrl: './sync-modal.component.html',
     styleUrls: ['./sync-modal.component.scss'],
     standalone: true,
-    imports: [IonicModule],
 })
 export class SyncModalComponent {}
