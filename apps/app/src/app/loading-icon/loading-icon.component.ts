1e73593ee44b2c5c5f09fa52666dfc6b2c2bddef
Onur Alp Bicer <onuralpbicer@gmail.com>
2023-05-08 23:46:30 +1000

    Added a loading

---
+++ apps/app/src/app/loading-icon/loading-icon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+import { IonicModule } from '@ionic/angular'
+
+@Component({
+    selector: 'beta-asset-app-loading-icon',
+    templateUrl: './loading-icon.component.html',
+    styleUrls: ['./loading-icon.component.scss'],
+    standalone: true,
+    imports: [IonicModule],
+})
+export class LoadingIconComponent {}
