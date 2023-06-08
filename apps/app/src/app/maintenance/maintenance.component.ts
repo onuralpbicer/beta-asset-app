af58fca2352dd12574f52ea1733b3757987d4b18
Onur Alp Bicer <onuralpbicer@gmail.com>
2023-06-08 22:31:58 +1000

    Maintenance page header

--- apps/app/src/app/maintenance/maintenance.component.ts
+++ apps/app/src/app/maintenance/maintenance.component.ts
@@ -1,12 +1,15 @@
 import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
 import { IonicModule } from '@ionic/angular'
+import { LoadingIconComponent } from '../loading-icon/loading-icon.component'
 
 @Component({
     selector: 'beta-asset-app-maintenance',
     templateUrl: './maintenance.component.html',
     styleUrls: ['./maintenance.component.scss'],
     standalone: true,
-    imports: [CommonModule, IonicModule],
+    imports: [CommonModule, IonicModule, LoadingIconComponent],
 })
-export class MaintenanceComponent {}
+export class MaintenanceComponent {
+    public loading = false
+}
