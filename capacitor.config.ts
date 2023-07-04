8c405ddcb28fb54c44b6a8bda9faaf0cbe1331d2
Onur Alp Bicer <onuralpbicer@gmail.com>
2023-07-04 21:34:29 +1000

    Updated app bundle id

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-    appId: 'io.ionic.starter',
+    appId: 'com.beta.bakim.app',
     appName: 'beta-asset-app',
     webDir: 'www',
     server: {
